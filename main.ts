674069515fce3f912361c3a2bb7cae2d23adc86f
Nick Rosier <github@bunbun.be>
2020-12-30 17:51:14 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+let ring = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
+ring.showRainbow(1, 360)
+basic.forever(function () {
+    ring.show()
+    ring.rotate(1)
+    basic.pause(100)
+})
